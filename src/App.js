970acf4640bfa8c0335fc3f7eb362cb0b8db81ad
achyutp10 <achyutp10@gmail.com>
2023-06-28 20:27:20 +0545

    Slight changes in UI

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ const App = () => {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
+      title: "Book",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
-      title: "Car Insurance",
+      title: "Chair",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
+      title: "Table",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
